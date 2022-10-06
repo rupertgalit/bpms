c4c994a6ebb8be693e7df926d8c2502757fb97f8
Rupert <galitrupert1522@gmail.com>
2022-10-06 12:07:37 +0800

    first commit

---
+++ admin/includes/dbconnection.php
@@ -0,0 +1,7 @@
+<?php
+$con=mysqli_connect("localhost", "u600599775_cd", "@Pfizer2022", "u600599775_abc");
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+
+  ?>
---
+++ admin/includes/footer.php
@@ -0,0 +1,5 @@
+<!--footer-->
+    <div class="footer">
+       <p>&copy; 2022 Rowena Salon Admin Panel.</p>
+    </div>
+        <!--//footer-->
